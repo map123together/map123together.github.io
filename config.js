e198471b1d1f7928ad5212208582f6af5c9d8df0
Charlie Gao <gcte2010@gmail.com>
2022-01-13 13:30:58 -0500

    layout changes

--- config.js
+++ config.js
@@ -34,6 +34,10 @@ function initMap() {
     makeUserBox(userBox);
     gMap.controls[google.maps.ControlPosition.TOP_LEFT].push(userBox);
 
+    var topToolBox = document.createElement('div');
+    makeTopToolBox(topToolBox);
+    gMap.controls[google.maps.ControlPosition.TOP_CENTER].push(topToolBox);
+
 
     // Listen for clicks and add the marker of the click.
     google.maps.event.addListener(gMap, "click", (e) => {
@@ -132,10 +136,9 @@ function makeInfoBox(controlDiv) {
                 <img style="
                 margin-left: auto;
                 margin-right: auto;
-                height: auto;
                 height: 40px;
-                width:auto;
-                " src="./Banner.png"
+                width: auto;
+                " src="./Banner-sm.png"
                 />
             </a>
         </div>`;//src="./ms-icon-144x144.png"
@@ -161,10 +164,46 @@ function makeUserBox(controlDiv) {
                 <img style="
                 margin-left: auto;
                 margin-right: auto;
-                height: auto;
-                height: 40px;
-                width: 40px;"
-                src="default-user.png"/>
+                margin-top: 1px;
+                height: 37px;
+                width: 37px;"
+                src="https://lh3.googleusercontent.com/a-/AOh14Giqx3_1q2THz1Z8M0KBOSBhKuWJROQL-3RA8T6iBqA=s96-c"
+                onerror="event.target.src = 'default-user.png';"/>
+            </div>
+        </div>`;
+    let controlUI = createElementFromHTML(userBox);
+    controlDiv.appendChild(controlUI);
+}
+
+function makeTopToolBox(controlDiv) {
+    let userBox = `
+        <div style="
+            border: 0px solid #ffffff;
+            border-radius: 2px;
+            box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 4px -1px; 
+            background-color:#ffffff;
+            margin-top: 10px;
+            margin-left: 10px;
+            margin-right: 10px;
+            margin-bottom: 22px;
+            height: 40px;
+            width: 185px;">
+            <div style="padding-top:5px; margin-left: 5px;"
+                class="btn-group-sm" role="group" aria-label="Basic radio toggle button group">
+                <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off">
+                <label class="btn btn-outline-primary" for="btnradio1"><i class="bi bi-dot"></i></label>
+            
+                <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+                <label class="btn btn-outline-primary" for="btnradio2"><i class="bi bi-dash-lg"></i></label>
+            
+                <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                <label class="btn btn-outline-primary" for="btnradio3"><i class="bi bi-diamond"></i></label>
+            
+                <input type="radio" class="btn-check" name="btnradio" id="btnradio4" autocomplete="off">
+                <label class="btn btn-outline-primary" for="btnradio4"><i class="bi bi-cursor-text"></i></label>
+            |
+                <input type="button" class="btn-check" name="btnradio" id="btnradio5" autocomplete="off">
+                <label class="btn btn-outline-primary" for="btnradio5"><i class="bi bi-send"></i></label>
             </div>
         </div>`;
     let controlUI = createElementFromHTML(userBox);
